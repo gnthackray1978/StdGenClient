9c2bf098a6b2be638f0b2aaadf22b6a807370218
George <gnthackray@btinternet.com>
2015-10-18 02:02:50 +0000

    title change

--- scripts/masters.js
+++ scripts/masters.js
@@ -214,7 +214,7 @@ JSMaster.prototype = {
         headersection += '<div id = "usr_nam"></div>';
         headersection += '</div>';
 
-        headersection += '<div class = "mtrlog">Generations Database</div>';
+        headersection += '<div class = "mtrlog">Personal Genealogy Database</div>';
         headersection += '<div id="errorDialog" title="Error"></div>';
 
         headersection += '</div>';
