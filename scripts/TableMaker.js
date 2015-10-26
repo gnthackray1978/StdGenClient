793a8a87f0b54a4c47fbd98ba86c49846f23b735
George <gnthackray@btinternet.com>
2015-10-26 15:15:14 +0000

    improved batch entry

--- scripts/TableMaker.js
+++ scripts/TableMaker.js
@@ -35,38 +35,38 @@ TableMaker.prototype = {
 
         this.selectorTools.addlinks(selectEvents, this.processSelect, this);
         
-        $('body').on("click", "." + this.tableId, function(evt){
-            console.log('testtesttest');
-            var colIdx = evt.target.parentElement.attributes["data-col"].value;
-            var idVal = evt.target.parentElement.attributes["data-id"].value;
+        // $('body').on("click", "." + this.tableId, function(evt){
+        //     console.log('testtesttest');
+        //     var colIdx = evt.target.parentElement.attributes["data-col"].value;
+        //     var idVal = evt.target.parentElement.attributes["data-id"].value;
             
-            switch (colIdx) {
-                case '0':
-                    break;
-                case '1':
-                    if(that.tableData.column1Func)
-                        that.tableData.column1Func(evt,idVal);
-                    break;
-                case '2':
-                    if(that.tableData.column2Func)
-                        that.tableData.column2Func(evt,idVal);
-                    break;
-                case '3':
-                    if(that.tableData.column3Func)
-                        that.tableData.column3Func(evt,idVal);
-                    break;
-                case '4':
-                    if(that.tableData.column4Func)
-                        that.tableData.column4Func(evt,idVal);
-                    break;
-                case '5':
-                    if(that.tableData.column5Func)
-                        that.tableData.column5Func(evt,idVal);
-                    break;
-            }
+        //     switch (colIdx) {
+        //         case '0':
+        //             break;
+        //         case '1':
+        //             if(that.tableData.column1Func)
+        //                 that.tableData.column1Func(evt,idVal);
+        //             break;
+        //         case '2':
+        //             if(that.tableData.column2Func)
+        //                 that.tableData.column2Func(evt,idVal);
+        //             break;
+        //         case '3':
+        //             if(that.tableData.column3Func)
+        //                 that.tableData.column3Func(evt,idVal);
+        //             break;
+        //         case '4':
+        //             if(that.tableData.column4Func)
+        //                 that.tableData.column4Func(evt,idVal);
+        //             break;
+        //         case '5':
+        //             if(that.tableData.column5Func)
+        //                 that.tableData.column5Func(evt,idVal);
+        //             break;
+        //     }
             
-            return false;
-        });
+        //     return false;
+        // });
         
     },
     
